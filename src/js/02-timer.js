6c7ca6e2d6a0c2b2a5b32563ffc4b05cb360aad7
TetianaTitarenko <110638769+TetianaTitarenko@users.noreply.github.com>
2023-01-06 00:06:42 +0000

    ms ?

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,27 +2,21 @@ import flatpickr from "flatpickr";
 // Додатковий імпорт стилів
 import "flatpickr/dist/flatpickr.min.css";
 
-// import { Notify } from 'notiflix/build/notiflix-notify-aio';
-// console.log(Notify);
-// Попередження
-// Notify.failure('Please choose a date in the future');
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 
 const input = document.querySelector("#datetime-picker");
+input.value = ``;
 const button = document.querySelector("button");
-console.log(button);
 const days = document.querySelector("[data-days]");
-console.log(days);
 const hours = document.querySelector("[data-hours]");
-console.log(hours);
 const minutes = document.querySelector("[data-minutes]");
-console.log(minutes);
 const seconds = document.querySelector("[data-seconds]");
-console.log(seconds);
 
 const options = {
   enableTime: true,
   time_24hr: true,
-  defaultDate: new Date(),
+  // defaultDate: new Date(),
     minuteIncrement: 1,
         locale: {
         firstDayOfWeek: 1
@@ -33,10 +27,55 @@ const options = {
 };
 
 const fp = flatpickr(input, options);
-console.log(fp.selectedDates[0]);
+
+const endDate = fp.selectedDates[0];
+console.log(endDate)
 
 
+button.addEventListener(`click`, onClickButton);
+input.addEventListener(`change`, addDate)
 
+function onClickButton() {
 
+  let timeId = setInterval(() => {
 
+    const currentDate = new Date();
+    console.log(currentDate)
+
+    // const ms = endDate.getTime() - currentDate.getTime();
+    const ms = endDate - currentDate;
+    console.log(ms);
+    
+    if (ms <= 0) {
+    clearInterval(timeId)
+          }
+  }, 1000)
+  
+};
+
+function addDate() {
+    if (fp.selectedDates[0] <= new Date()) {
+    Notify.failure('Please choose a date in the future');
+  }
+  return
+}
+
+// function convertMs(ms) {
+//   // Number of milliseconds per unit of time
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
+
+//   // Remaining days
+//   const days = Math.floor(ms / day);
+//   // Remaining hours
+//   const hours = Math.floor((ms % day) / hour);
+//   // Remaining minutes
+//   const minutes = Math.floor(((ms % day) % hour) / minute);
+//   // Remaining seconds
+//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+//   return { days, hours, minutes, seconds };
+// }
 
