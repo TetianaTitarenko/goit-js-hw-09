5cf5ff19fc4562d89eb040f39db062b806aee4a6
TetianaTitarenko <110638769+TetianaTitarenko@users.noreply.github.com>
2023-01-09 12:56:32 +0000

    not console

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,22 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+const refs = {
+  firstDelay: document.querySelector(`input[name='delay']`),
+  delayStep: document.querySelector(`input[name='step']`),
+  amount: document.querySelector(`input[name='amount']`),
 }
+console.log(refs);
+
+const createBtn = document.querySelector(`button`)
+console.log(createBtn)
+
+
+
+
+
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }
